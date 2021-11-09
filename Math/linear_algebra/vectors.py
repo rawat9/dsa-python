c7a2ffc7d32a9e659a40368c1c6d82d04088a1c9
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-11-09 22:16:10 +0000

    + vectors.py ⚙️

--- Math/linear_algebra/vectors.py
+++ Math/linear_algebra/vectors.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import sqrt
+from math import acos, sqrt
 from numpy.linalg import eig, det
 
 
@@ -26,21 +26,40 @@ class Vector:
         return det(np.array(m))
 
     def dot_product(self, u: list, v: list) -> int:
+        """
+        Calculate the dot product of two vectors u, v
+        """
         return np.dot(u, v)
 
     def cross_product(self, u: list, v: list) -> int:
+        """
+        Calculate the cross product of two vectors u, v
+        """
         return np.cross(u, v)
 
     def magnitude(self, u: list) -> float:
-        return sqrt(sum(list(map(lambda x: x**2, np.array(u)))))
+        """
+        Calculate the magnitude of vector u
+        """
+        return sqrt(sum(list(map(lambda x: x ** 2, np.array(u)))))
+
+    def angle(self, u, v):
+        """
+        Angle between two vectors: u, v in radians
+        """
+        return acos(self.dot_product(u, v) / (self.magnitude(u) * self.magnitude(v)))
 
     def eigen(self, m: list[list]):
+        """
+        Calculate eigenvalues and eigenvectors of
+        """
         eigenvalues, eigenvectors = eig(np.array(m))
         np.set_printoptions(precision=4)
-        print('Eigenvalues:' , eigenvalues)
-        print('Eigenvectors:', eigenvectors)
+        print("Eigenvalues:", eigenvalues)
+        print("Eigenvectors:", eigenvectors)
 
 
-if __name__ == '__main__':  
+if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
