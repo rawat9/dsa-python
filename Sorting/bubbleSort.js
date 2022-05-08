5861098435ce2729f817c6147fc34c8c64b62746
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-05-08 23:19:22 +0100

    + algo/sorting bubbleSort.js

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+const arr = [6, 5, 3, 1, 8, 7, 2, 4]
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+}
+
+bubbleSort(arr)
+console.log(arr)
