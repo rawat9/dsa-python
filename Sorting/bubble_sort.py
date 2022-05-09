a2394cd897e954f474d3bd172573ca7569a6d429
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-05-09 16:39:34 +0100

    + algo/sorting enhanced bubble_sort.py

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -2,10 +2,16 @@ from typing import List
 
 
 def bubble_sort(arr: List[int]):
-    for _ in range(len(arr)):
-        for j in range(len(arr) - 1):
+    n = len(arr)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(n - 1 - i):
             if arr[j] > arr[j + 1]:
+                swapped = True
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+        if not swapped:
+            break
 
 
 if __name__ == "__main__":
